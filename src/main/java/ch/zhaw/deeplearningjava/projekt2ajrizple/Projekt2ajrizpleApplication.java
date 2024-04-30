f3d5bdbd812b94c2aa78533526c9d83b9e5e30a6
pleuempor <118727993+pleuempor@users.noreply.github.com>
2024-05-01 00:05:58 +0200

    first commit

---
+++ src/main/java/ch/zhaw/deeplearningjava/projekt2ajrizple/Projekt2ajrizpleApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.deeplearningjava.projekt2ajrizple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekt2ajrizpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projekt2ajrizpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/zhaw/deeplearningjava/projekt2ajrizple/Projekt2ajrizpleApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.deeplearningjava.projekt2ajrizple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projekt2ajrizpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
